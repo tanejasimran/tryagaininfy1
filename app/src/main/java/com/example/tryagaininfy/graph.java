2c54856122b85944fe8c44f34b422d44b99ba08e
simra <tanejasimran2222@gmail.com>
2020-07-26 17:39:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tryagaininfy/graph.java
@@ -0,0 +1,14 @@
+package com.example.tryagaininfy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class graph extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
